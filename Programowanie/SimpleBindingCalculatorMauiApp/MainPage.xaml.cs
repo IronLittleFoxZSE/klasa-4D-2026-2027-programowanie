f29f0573c721657e69a06788aeca2208332dfbd1
Zse <zse@zse.pl>
2026-09-22 12:44:36 +0200

    koniec lekcji - omówione kontenery i dodany projekt bindującego kalkulatora

---
+++ Programowanie/SimpleBindingCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace SimpleBindingCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public string FirstStrNumber { get; set; }
+
+        public string SecondStrNumber { get; set; }
+
+        private string result;
+        public string Result
+        {
+            get { return result; }
+            set { result = value; OnPropertyChanged(); }
+        }
+
+        private Color colorResult;
+        public Color ColorResult
+        {
+            get { return colorResult; }
+            set { colorResult = value; OnPropertyChanged(); }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(FirstStrNumber, out int firstNumber)
+                && int.TryParse(SecondStrNumber, out int secondNumber))
+            {
+                int result = firstNumber + secondNumber;
+                Result = "Wynik to " + result;
+                ColorResult = Colors.Green;
+            }
+            else
+            {
+                Result = "Podałeś nieprawidłowe dane";
+                ColorResult = Colors.Red;
+            }
+        }
+    }
+}
